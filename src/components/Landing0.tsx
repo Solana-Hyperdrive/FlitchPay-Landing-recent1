799cbaf6dea109fab7b5efe5620d2bf0451a989d
Krishav <krishavrajsingh@gmail.com>
2023-10-19 00:05:22 +0530

    improved responsiveness

--- src/components/Landing0.tsx
+++ src/components/Landing0.tsx
@@ -1,32 +1,32 @@
-export default function Landing0(){
-    return (
-        <section style={{ height: "100vh", width: "100%", background: "radial-gradient(70.32% 109% at 98.17% 55.75%, #401694 11.37%, #000 100%)" }} className="p-8 relative overflow-hidden">
-          <div className="border-solid border-gray-500 rounded-xl border-2 p-10 h-full">
-            <div className="w-[28rem] p-4">
-              <div className="font-bold text-4xl text-white leading-10">Building the Future of <span className="" style={{background: "linear-gradient(136deg, #9C74F1 52.45%, #3600A8 73.07%)", WebkitBackgroundClip: "text", backgroundClip: "text", WebkitTextFillColor: "transparent"}}>Decentralized</span> Payments </div>
-              <div className="text-gray-600 mt-2 text-sm bg-gradient-to-r from-slate-500 to-white text-transparent bg-clip-text"> Solana-run technology powering the cryptocurrency payments network Flitch-Pay and decentralized applications.</div>
-              <a href="/" className="hover:bg-violet-600 rounded-full"><button style={{borderRadius: "7rem", border: "0.5px solid #775CF0", boxShadow: "0px 0px 2.22777px 0px rgba(255, 255, 255, 0.25) inset"}} className=" backdrop-filter-none rounded-full border-2 border-violet-600 bg-black fill-black mt-4 py-1 px-4  text-white hover:bg-violet-600">
-              Get Started for Free
-            </button></a>
-              
-          </div>
-          <div className="h-full w-full flex">
-            <div className="relative left-[10rem] bottom-[3rem] ">
-              <img className="h-[35rem] aspect-auto block shrink-0" src="https://cdn.discordapp.com/attachments/1159478783020113980/1163837366197309553/Group_39574.webp?ex=65410703&is=652e9203&hm=7e86ef8ee152c073efdae7084634b97025df8b7aab215120aa3162d566a27a68&" alt="" />
-            </div>
-            <div className="relative left-[7rem] bottom-[13rem]">
-              <img className="h-[43rem] aspect-auto block " src="https://cdn.discordapp.com/attachments/1159478783020113980/1163837366608330803/Group_39575.webp?ex=65410703&is=652e9203&hm=c8dcc2707379ae2c3f3756426645cdd34bcf7880825d51731531bc9e33a7cc18&" alt="" />
-            </div>
-    
-          </div>
-          
-    
-    
-    
+export default function Landing0() {
+  return (
+    <section style={{ height: "100vh", width: "100%", background: "radial-gradient(70.32% 109% at 98.17% 55.75%, #401694 11.37%, #000 100%)" }} className="p-8 relative overflow-hidden">
+      <div className="border-solid border-gray-500 rounded-xl border-2 h-full p-3">
+        <div className="m-5 sm:w-[25rem] sm:m-8">
+          <div className="font-bold text-3xl text-white">Building the Future of <span className="" style={{ background: "linear-gradient(136deg, #9C74F1 52.45%, #3600A8 73.07%)", WebkitBackgroundClip: "text", backgroundClip: "text", WebkitTextFillColor: "transparent" }}>Decentralized</span> Payments </div>
+          <div className="text-gray-600 mt-2 text-xs sm:text-sm bg-gradient-to-r from-slate-500 to-white text-transparent bg-clip-text"> Solana-run technology powering the cryptocurrency payments network Flitch-Pay and decentralized applications.</div>
+          <a href="/" className="hover:bg-violet-600 rounded-full"><button style={{ borderRadius: "7rem", border: "0.5px solid #775CF0", boxShadow: "0px 0px 2.22777px 0px rgba(255, 255, 255, 0.25) inset" }} className=" backdrop-filter-none rounded-full border-2 border-violet-600 bg-black fill-black mt-4 py-1 px-4  text-white hover:bg-violet-600">
+            Get Started for Free
+          </button></a>
+
         </div>
-    
-        
-    
-        </section >
-      )
+        <div className="h-full w-full flex sm:grid-cols-8">
+          <div className="absolute left-[-2rem] bottom-[-5rem] xl:left-[14rem] xl:bottom-[-12rem]">
+            <img className="w-[15rem] xl:w-[37rem] aspect-auto block shrink-0" src="https://cdn.discordapp.com/attachments/1159478783020113980/1163837366197309553/Group_39574.webp?ex=65410703&is=652e9203&hm=7e86ef8ee152c073efdae7084634b97025df8b7aab215120aa3162d566a27a68&" alt="" />
+          </div>
+          <div className="absolute right-[-4rem] bottom-[-3rem] xl:bottom-[-3rem] xl:right-[-8rem]">
+            <img className="w-[18rem] aspect-auto xl:w-[38rem]" src="https://cdn.discordapp.com/attachments/1159478783020113980/1163837366608330803/Group_39575.webp?ex=65410703&is=652e9203&hm=c8dcc2707379ae2c3f3756426645cdd34bcf7880825d51731531bc9e33a7cc18&" alt="" />
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+    </section >
+  )
 }
