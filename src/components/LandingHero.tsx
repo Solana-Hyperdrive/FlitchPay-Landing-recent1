b79fd3776b4d6ee53630cf11612f08e4faec76eb
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-11-05 10:47:02 +0530

    few bugs fixed in layout and hero section

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -11,36 +11,40 @@ export default function LandingHero() {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
-    <section className="w-full bg-background py-12 md:py-24 lg:py-32 ">
-      <div className="container w-fit md:mx-64 md:px-6">
-        <Button
-          variant={"primary"}
-          size={"sm"}
-          className="my-3 flex items-center gap-1 h-6 rounded-xl"
-        >
-          {/* logo */}
-          <div>Check our new update</div>
-          <ArrowRight size={"1rem"} />
-        </Button>
-        <div className="flex flex-wrap justify-between">
-          <div className="text-5xl tracking-wide md:w-7/12 md:text-6xl md:leading-tight">
-            Build like a team <br />
-            of hundreds<span className="text-primary">_</span>
+    <section className="flex items-center bg-background py-12 md:py-24 lg:py-32 ">
+      <div className="container mx-auto flex flex-col items-center md:px-36 ">
+        <div>
+          <div className="md:px-2">
+            <Button
+              variant={"primary"}
+              size={"sm"}
+              className="my-3 flex h-6 items-center gap-1 rounded-xl"
+            >
+              {/* logo */}
+              <div>Check our new update</div>
+              <ArrowRight size={"1rem"} />
+            </Button>
           </div>
-          <div className="mt-3 flex flex-col gap-6 md:w-5/12 ">
-            <p className="text-muted-foreground">
-              {"Appwrite's"} open-source platform lets you add Auth, DBs,
-              Functions and Storage to your product and build any application at
-              any scale, own your data, and use your preferred coding languages
-              and tools.
-            </p>
-            <div className="flex gap-6">
-              <Button size={"sm"} variant={"primary"}>
-                Get Started
-              </Button>
-              <Button variant={"outline"} size={"sm"}>
-                Join the waitlist
-              </Button>
+          <div className="flex flex-wrap justify-between">
+            <div className="text-5xl tracking-wide md:w-1/2 md:px-2 md:text-6xl md:leading-tight">
+              Build like a team of hundreds
+              <span className="text-primary">_</span>
+            </div>
+            <div className="mt-3 flex flex-col justify-between gap-6 px-2 md:w-1/2 ">
+              <p className="text-muted-foreground text-base">
+                {"Appwrite's"} open-source platform lets you add Auth, DBs,
+                Functions and Storage to your product and build any application
+                at any scale, own your data, and use your preferred coding
+                languages and tools.
+              </p>
+              <div className="flex gap-6">
+                <Button size={"sm"} variant={"primary"}>
+                  Get Started
+                </Button>
+                <Button variant={"outline"} size={"sm"}>
+                  Join the waitlist
+                </Button>
+              </div>
             </div>
           </div>
         </div>
